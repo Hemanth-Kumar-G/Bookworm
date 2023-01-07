8a24303b4da44223c6f668998cdbcd5182935a2f
Hemanth Kumar <70285394+Hemanth-Kumar-G@users.noreply.github.com>
2023-01-07 12:55:55 +0530

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by HEMANTH on 07/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
